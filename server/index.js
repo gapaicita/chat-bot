66cb449b9a84ec53f488bed5787f10bea75b58a9
gapaicita <hanifchusnul@gmail.com>
2023-11-10 16:36:45 -0800

    change configuration API Request

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+
 
 const app = express();
 app.use(cors());
@@ -23,14 +23,12 @@ app.get("/", async (req, res) => {
 app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
+    const response = await openai.chat.completion.create({
+      model: "gpt-3.5-turbo",
+      messages: [`${prompt}`],
       temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
+      max_tokens: 1024,
+     
     });
     res.status(200).send({
       bot: response.data.choices[0].text,
